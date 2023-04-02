214eabe5bc5bae9f3b07fecc8b3481f2bfe26d34
Zane <alessandro.demaio@virgilio.it>
2023-04-02 20:03:51 +0200

    fist commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+// import './App.css';
+import './index.css';
 import motokoLogo from './assets/motoko_moving.png';
 import motokoShadowLogo from './assets/motoko_shadow.png';
 import reactLogo from './assets/react.svg';
+import {
+  createRoutesFromElements, Link, createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
 import { backend } from './declarations/backend';
 
 function App() {
@@ -39,42 +45,21 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://internetcomputer.org/docs/current/developer-docs/build/cdks/motoko-dfinity/motoko/"
-          target="_blank"
-        >
-          <span className="logo-stack">
-            <img
-              src={motokoShadowLogo}
-              className="logo motoko-shadow"
-              alt="Motoko logo"
-            />
-            <img src={motokoLogo} className="logo motoko" alt="Motoko logo" />
-          </span>
-        </a>
-      </div>
-      <h1>Vite + React + Motoko</h1>
-      <div className="card">
-        <button onClick={increment} style={{ opacity: loading ? 0.5 : 1 }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>backend/Main.mo</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite, React, and Motoko logos to learn more
-      </p>
+    <div className="App bg-gray-900 w-screen h-screen">
+
     </div>
   );
 }
 
-export default App;
+// export default App;
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/">
+    <Route index element={<App />} />
+  </Route>
+));
+
+
+export default () => (
+  <RouterProvider router={router} />
+)
